ed1cd89456302ce69788b2a5e8c5f61d73c73916
Zoe Carmona <zocaya2016@gmail.com>
2026-01-06 12:40:34 -0600

    Adding Ex11

---
+++ Arrays/A11.py
@@ -0,0 +1,14 @@
+# Problem
+# Return an array where each element is the running sum.
+
+# Input:  [1,2,3,4]
+# Output: [1,3,6,10]
+def running_sum(nums):
+    sum = nums[0]
+    for n in range(1, len(nums)):
+        sum += nums[n]
+        nums[n] = sum
+    return nums
+
+nums = [1,2,3,4]
+print(running_sum(nums))
