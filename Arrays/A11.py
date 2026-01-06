f9c2adcc737788085587b36387fd50bcf61013ac
Zoe Carmona <zocaya2016@gmail.com>
2026-01-06 12:42:34 -0600

    Running Sum

--- Arrays/A11.py
+++ Arrays/A11.py
@@ -1,8 +1,8 @@
 # Problem
 # Return an array where each element is the running sum.
 
-# Input:  [1,2,3,4]
-# Output: [1,3,6,10]
+# Input:  [1,2,3,4,5]
+# Output: [1,3,6,10,15]
 def running_sum(nums):
     sum = nums[0]
     for n in range(1, len(nums)):
@@ -10,5 +10,5 @@ def running_sum(nums):
         nums[n] = sum
     return nums
 
-nums = [1,2,3,4]
+nums = [1,2,3,4,5]
 print(running_sum(nums))
