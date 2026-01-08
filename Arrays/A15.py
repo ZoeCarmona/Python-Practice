b59ca7bf6a60fbbff52327ca99c1e672785d3f87
Zoe Carmona <zocaya2016@gmail.com>
2026-01-08 11:34:50 -0600

    Product of Array Except Self

--- Arrays/A15.py
+++ Arrays/A15.py
@@ -9,5 +9,31 @@
 
 # Input:  [1,2,3,4]
 # Output: [24,12,8,6]
+# def product_except_self(nums):
+#     productRes = 1
+#     product = []
+#     for i in range(len(nums)):
+#         for j in range(len(nums)):
+#             if i != j:
+#                 productRes *= nums[j]
+#         product.append(productRes)
+#         productRes = 1
+#     return product
 def product_except_self(nums):
-    pass
+    n = len(nums)
+    result = [1] * n
+
+    prefix = 1
+    for i in range(n):
+        result[i] = prefix
+        prefix *= nums[i]
+
+    suffix = 1
+    for i in range(n - 1, -1, -1):
+        result[i] *= suffix
+        suffix *= nums[i]
+
+    return result
+
+nums = [1,2,3,4]
+print(product_except_self(nums))
