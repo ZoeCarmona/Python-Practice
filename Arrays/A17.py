1bed664fb5dcc122a07658ad567a72e2c5cb1e87
Zoe Carmona <zocaya2016@gmail.com>
2026-01-08 12:37:54 -0600

    Find First Duplicate

--- Arrays/A17.py
+++ Arrays/A17.py
@@ -1,8 +1,13 @@
-# Problem: Container With Most Water
-# Given heights, return the max area of water a container can store.
+# Problem: Find First Duplicate
+# Return the first duplicate number you encounter while scanning from left to right.
 
-# Input:  [1,8,6,2,5,4,8,3,7]
-# Output: 49
-
-def max_area(height):
-    pass
+# Input:  [2, 1, 3, 5, 3, 2]
+# Output: 3
+def first_duplicate(nums):
+    seen = set()
+    for n in nums:
+        if n in seen:
+            return n
+        seen.add(n)
+nums = [2, 1, 3, 5, 4, 6]
+print(first_duplicate(nums))
