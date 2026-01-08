1ef3447d562c3d177faa7d89ec1fd0caabc82b4c
Zoe Carmona <zocaya2016@gmail.com>
2026-01-08 12:08:29 -0600

    Container With Most Water

--- Arrays/A16.py
+++ Arrays/A16.py
@@ -1,7 +1,26 @@
-# Problem: Maximum Subarray
-# Find the contiguous subarray with the largest sum.
+# Problem: Container With Most Water
+# Given heights, return the max area of water a container can store.
 
-# Input:  [-2,1,-3,4,-1,2,1,-5,4]
-# Output: 6   # [4,-1,2,1]
-def max_subarray(nums):
-    pass
+# Input:  [1,8,6,2,5,4,8,3,7]
+# Output: 49
+
+def max_area(height):
+    left = 0
+    right = len(height) - 1
+    maxArea = 0
+
+    while left < right:
+        b = right - left
+        h = min(height[left], height[right])
+        area = b * h
+        maxArea = max(maxArea, area)
+
+        if height[left] < height[right]:
+            left += 1
+        else:
+            right -= 1
+
+    return maxArea
+
+height = [1,8,6,2,5,4,8,3,7]
+print(max_area(height))
