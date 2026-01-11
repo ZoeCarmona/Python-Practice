20c836e89034993ca87bca59f1eaf6c7cb243590
Zoe Carmona <zocaya2016@gmail.com>
2026-01-10 22:19:28 -0600

    1P

---
+++ Arrays/A27.py
@@ -0,0 +1,15 @@
+# Peak Element
+
+# Problem
+# A peak element is an element strictly greater than its neighbors.
+# Return the index of any peak element.
+
+# You may assume:
+
+# nums[-1] = nums[n] = -∞
+
+# Input:  [1,2,3,1]
+# Output: 2   # index of 3
+
+def find_peak_element(nums):
+    pass
