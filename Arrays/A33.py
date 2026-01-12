2614f1831748aaa6c560c9f2c1a869fba1c4a58f
Zoe Carmona <zocaya2016@gmail.com>
2026-01-11 22:07:19 -0600

    Pending

---
+++ Arrays/A33.py
@@ -0,0 +1,13 @@
+# Minimum Number of Arrows to Burst Balloons
+
+# Pattern: Sorting + greedy
+
+# Problem
+# Each balloon is an interval [start, end]. One arrow at position x bursts all balloons where start <= x <= end.
+# Return the minimum number of arrows needed.
+
+# Input:  [[10,16],[2,8],[1,6],[7,12]]
+# Output: 2
+
+def find_min_arrow_shots(points):
+    pass
