7c24bf8102afe5c0a68a908f0e639b34c5b7ec73
Zoe Carmona <zocaya2016@gmail.com>
2026-01-12 10:59:41 -0600

    Merge Sorted Array (In-place)

--- Arrays/A30.py
+++ Arrays/A30.py
@@ -1,8 +1,4 @@
-# Merge Sorted Array (In-place)
-
-# Pattern: Two pointers from the end
-
-# Problem
+# Problem: Merge Sorted Array (In-place)
 # You’re given nums1 with enough extra space at the end to hold nums2.
 # Merge nums2 into nums1 in sorted order.
 
@@ -13,4 +9,22 @@
 # [1,2,2,3,5,6]
 
 def merge(nums1, m, nums2, n):
-    pass
+    i = m - 1      
+    j = n - 1      
+    k = m + n - 1  
+
+    while j >= 0:
+        if i >= 0 and nums1[i] > nums2[j]:
+            nums1[k] = nums1[i]
+            i -= 1
+        else:
+            nums1[k] = nums2[j]
+            j -= 1
+        k -= 1
+
+nums1 = [1,2,3,0,0,0]
+m = 3
+nums2 = [2,5,6]
+n = 3
+
+print(merge(nums1, m, nums2, n))
